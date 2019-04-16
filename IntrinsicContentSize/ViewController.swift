5af66c7d18c58d6397108c8616f64c71d0064aa3
Liliya <lilyeka@gmail.com>
2019-04-16 16:50:17 +0500

    project

--- IntrinsicContentSize/ViewController.swift
+++ IntrinsicContentSize/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var textField1 = UITextField()
+    var textField2 = UITextField()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        textField1.translatesAutoresizingMaskIntoConstraints = false
+        textField2.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField1.isUserInteractionEnabled = true
+        textField2.isUserInteractionEnabled = true
+        
+        textField1.text = "1"
+        textField1.backgroundColor = .green
+        textField2.text = "2"
+        textField2.backgroundColor = .yellow
+        
+        self.view.addSubview(textField1)
+        self.view.addSubview(textField2)
+        
+        let d = ["v1":textField1,"v2":textField2]
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint.constraints(withVisualFormat:
+                "V:|-120-[v1]", metrics: nil, views: d),
+            NSLayoutConstraint.constraints(withVisualFormat:
+                "V:|-120-[v2]", metrics: nil, views: d),
+            NSLayoutConstraint.constraints(withVisualFormat:
+                "H:|-20-[v1]", metrics: nil, views: d),
+            NSLayoutConstraint.constraints(withVisualFormat:
+                "H:[v2]-20-|", metrics: nil, views: d),
+            NSLayoutConstraint.constraints(withVisualFormat:
+                "H:[v1(>=100)]-(>=20)-[v2(>=100)]", metrics: nil, views: d)
+            ].flatMap{$0})
+        
+        let p = textField1.contentCompressionResistancePriority(for: .horizontal)
+        textField2.setContentCompressionResistancePriority(p+1, for: .horizontal)
     }
 
 
