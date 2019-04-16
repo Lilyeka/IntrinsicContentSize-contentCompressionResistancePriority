e2db3c546556b035fc1938e050f2c8798f6b28fa
Liliya <lilyeka@gmail.com>
2019-04-16 16:29:58 +0500

    Initial Commit

---
+++ IntrinsicContentSize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntrinsicContentSize
+//
+//  Created by Лилия Левина on 16/04/2019.
+//  Copyright © 2019 Лилия Левина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
